7a54ba1d459c01dc6f297f35e0737e8ee5170e5c
lmatsuki <lmatsuki87@gmail.com>
2019-03-27 23:10:07 -0700

    Added listbox and statusbar modules

---
+++ src/PDFMerger.Wpf/Modules/StatusBar.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace PDFMerger.Wpf.Modules
+{
+    /// <summary>
+    /// Interaction logic for StatusBar.xaml
+    /// </summary>
+    public partial class StatusBar : UserControl
+    {
+        public StatusBar()
+        {
+            InitializeComponent();
+        }
+
+        private void txtEditor_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            statusBar.Text = "Drop PDF-documents in the box above, or choose \"Add file...\" from the toolbar";
+        }
+    }
+}
