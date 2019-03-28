264e00d6b454296ef6a8494ed946cc436ee78fae
lmatsuki <lmatsuki87@gmail.com>
2019-03-27 21:24:24 -0700

    Bound application startup and exit methods

--- src/PDFMerger.Wpf/App.xaml.cs
+++ src/PDFMerger.Wpf/App.xaml.cs
@@ -13,5 +13,21 @@ namespace PDFMerger.Wpf
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            StartupUri = new Uri(@"Windows\MainWindow.xaml", UriKind.Relative);
+
+            base.OnStartup(e);
+        }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+
+        }
+
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+
+        }
     }
 }
