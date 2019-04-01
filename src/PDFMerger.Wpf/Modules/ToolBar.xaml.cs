26a717ed08a0031a16818ccf5469cfaff505cf7f
lmatsuki <lmatsuki87@gmail.com>
2019-03-31 23:54:37 -0700

    Added custom commands

--- src/PDFMerger.Wpf/Modules/ToolBar.xaml.cs
+++ src/PDFMerger.Wpf/Modules/ToolBar.xaml.cs
@@ -36,5 +36,54 @@ namespace PDFMerger.Wpf.Modules
         }
 
         #endregion
+
+        #region RemoveFile Command
+
+        private void RemoveFileCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = false;
+        }
+
+        private void RemoveFileCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Remove file!");
+        }
+
+        #endregion
     }
+
+    #region Custom Commands
+
+    public static class CustomCommands
+    {
+        public static readonly RoutedUICommand RemoveFile = new RoutedUICommand
+        (
+            "RemoveFile",
+            "RemoveFile",
+            typeof(CustomCommands)
+        );
+
+        public static readonly RoutedUICommand MoveUp = new RoutedUICommand
+        (
+            "MoveUp",
+            "MoveUp",
+            typeof(CustomCommands)
+        );
+
+        public static readonly RoutedUICommand MoveDown = new RoutedUICommand
+        (
+            "MoveDown",
+            "MoveDown",
+            typeof(CustomCommands)
+        );
+
+        public static readonly RoutedUICommand BindFile = new RoutedUICommand
+        (
+            "BindFile",
+            "BindFile",
+            typeof(CustomCommands)
+        );
+    }
+
+    #endregion
 }
