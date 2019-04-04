2b54f32ad6e33b92c18b5c4667a559335d6ec5e8
lmatsuki <lmatsuki87@gmail.com>
2019-04-03 22:43:17 -0700

    Added placeholder Mediator class

---
+++ src/PDFMerger.Wpf/SharedKernel/Mediator.cs
@@ -0,0 +1,21 @@
+﻿using PDFMerger.Wpf.Modules;
+
+namespace PDFMerger.Wpf.SharedKernel
+{
+    public class Mediator
+    {
+        private ToolBar toolBar;
+        private ListBox listBox;
+        public Mediator(ToolBar toolBar, ListBox listBox)
+        {
+            this.toolBar = toolBar;
+            this.listBox = listBox;
+        }
+
+        public void Send(object message)
+        {
+            toolBar.Notify(message);
+            listBox.Notify(message);
+        }
+    }
+}
