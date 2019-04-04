01437fed80635cbbcefd6e73469c1ecaac2387ef
lmatsuki <lmatsuki87@gmail.com>
2019-04-03 22:44:28 -0700

    Added view models

---
+++ src/PDFMerger.Wpf/ViewModels/MergeFilesViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.ObjectModel;
+
+namespace PDFMerger.Wpf.ViewModels
+{
+    public class MergeFilesViewModel
+    {
+        public ObservableCollection<string> MergeFiles { get; set; } = new ObservableCollection<string>();
+    }
+}
